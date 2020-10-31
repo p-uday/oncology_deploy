f135b670d8fac7a185f9f46f90402eb76d652811
p-uday <64903756+p-uday@users.noreply.github.com>
2020-10-31 12:24:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,12 @@ def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
+    prediction_prob=model.predict_proba(final_features)
 
     output = prediction[0]
     prediction_benign=""
     prediction_malignant=""
+    confidence=round(np.max(prediction_prob)*100,2)
     if(output==0):
         prediction_benign='Tumour is Benign'
         statement='Nothing To Worry'
@@ -28,7 +30,7 @@ def predict():
         prediction_malignant="Tumour is Malignant"
         statement='Please Consult Doctor'
 
-    return render_template('predict.html', prediction_benign=prediction_benign,prediction_malignant=prediction_malignant,statement=statement)
+    return render_template('predict.html', prediction_benign=prediction_benign,prediction_malignant=prediction_malignant,statement=statement,confidence="{}% Confidence".format(confidence))
 
 
 if __name__ == "__main__":
